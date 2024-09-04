45a4dfd769b3a8d7e20db6f6d1599530d6a1179f
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-04 21:51:38 +0900

    Create model sprint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import { Hono } from 'hono';
+import { getPrisma } from './prismaFunction';
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
 
+app.post('/sprint/create', async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const sprint = await prisma.sprint.create({
+    data: {
+      subject: 'Alice is a friend of mine',
+      content: 'She is a good friend, and I like her a lot.',
+      hash: 'alice-friend',
+    },
+  });
+  console.log(sprint);
+});
 export default app;
