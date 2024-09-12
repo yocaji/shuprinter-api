d9b7a8e25c1569c758383d9ad0765cf9a260344a
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-12 09:03:17 +0900

    Add /notes/:key and CORS support

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { getPrisma } from './prismaFunction';
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    CORS_ORIGIN: string;
   };
 }>();
+
+app.use('/*', async (c, next) => {
+  const corsMiddlewareHandler = cors({
+    origin: c.env.CORS_ORIGIN,
+    allowMethods: ['GET', 'POST'],
+    maxAge: 86400,
+  });
+  return corsMiddlewareHandler(c, next);
+});
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
 
-app.post('/sprint/create', async (c) => {
+app.get('/notes/:key', async (c) => {
+  const { key } = c.req.param();
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const note = await prisma.sprint.findFirst({
+    where: {
+      hash: key,
+    },
+  });
+  return c.json(note);
+});
+
+app.post('/notes/create', async (c) => {
   const prisma = getPrisma(c.env.DATABASE_URL);
   const sprint = await prisma.sprint.create({
     data: {
