1fc9ef47b7ce589edb52931973876323fd50dc2d
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-14 23:20:14 +0900

    Update schema.prisma

--- prisma/migrations/20240904124113_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Sprint" (
-    "id" SERIAL NOT NULL,
-    "subject" TEXT NOT NULL,
-    "content" TEXT,
-    "hash" TEXT NOT NULL,
-
-    CONSTRAINT "Sprint_pkey" PRIMARY KEY ("id")
-);
