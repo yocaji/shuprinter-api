45e31911f26861f99e71b8913df896da82fa2de0
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-14 23:20:48 +0900

    Introduce zod-openapi

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { Env, Hono } from 'hono';
-import { cors } from 'hono/cors';
 import { getPrisma } from './prismaFunction';
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { cors } from 'hono/cors';
+import { welcomeRoute, noteRoute, createNoteRoute } from './route';
 
-const app = new Hono<{
-  Bindings: {
-    DATABASE_URL: string;
-    CORS_ORIGIN: string;
-  };
-}>() satisfies ExportedHandler<Env>;
+type Bindings = {
+  DATABASE_URL: string;
+  CORS_ORIGIN: string;
+};
+
+const app = new OpenAPIHono<{
+  Bindings: Bindings;
+}>();
 
 app.use('/*', async (c, next) => {
   const corsMiddlewareHandler = cors({
@@ -18,31 +21,66 @@ app.use('/*', async (c, next) => {
   return corsMiddlewareHandler(c, next);
 });
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.openapi(welcomeRoute, (c) => {
+  return c.json({ message: 'Welcome to SprintPost!' }, 200);
 });
 
-app.get('/notes/:key', async (c) => {
-  const { key } = c.req.param();
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  const note = await prisma.note.findFirst({
-    where: {
-      key: key,
-    },
-  });
-  return c.json(note);
+app.openapi(
+  noteRoute,
+  async (c) => {
+    const { noteKey } = c.req.valid('param');
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const note = await prisma.note.findFirst({
+      select: {
+        noteKey: true,
+        subject: true,
+        content: true,
+      },
+      where: {
+        noteKey,
+      },
+    });
+    if (!note) {
+      return c.json({ code: 404, message: 'Not Found' }, 404);
+    } else {
+      return c.json(note, 200);
+    }
+  },
+  (result, c) => {
+    return c.json({ code: 400, message: 'Validation Error' }, 400);
+  },
+);
+
+app.openapi(
+  createNoteRoute,
+  async (c) => {
+    const { noteKey, subject, content } = c.req.valid('json');
+
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const note = await prisma.note.create({
+      data: {
+        noteKey,
+        subject,
+        content,
+      },
+    });
+    if (!note) {
+      return c.json({ code: 400, message: 'Bad Request' }, 400);
+    } else {
+      return c.json(note, 201);
+    }
+  },
+  (result, c) => {
+    return c.json({ code: 400, message: 'Validation Error' }, 400);
+  },
+);
+
+app.doc31('/doc', {
+  openapi: '3.1.0',
+  info: {
+    version: '1.0.0',
+    title: 'SprintPost API',
+  },
 });
 
-app.post('/notes/create', async (c) => {
-  const { subject, key } = await c.req.json();
-
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  const note = await prisma.note.create({
-    data: {
-      subject: subject,
-      key: key,
-    },
-  });
-  return c.json(note);
-});
 export default app;
---
+++ src/route.ts
@@ -0,0 +1,89 @@
+import { createRoute } from '@hono/zod-openapi';
+import {
+  WelcomeSchema,
+  NoteParamsSchema,
+  NoteSchema,
+  ErrorSchema,
+} from './schema';
+
+export const welcomeRoute = createRoute({
+  method: 'get',
+  path: '/',
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: WelcomeSchema,
+        },
+      },
+      description: 'Hello Hono!',
+    },
+  },
+});
+
+export const createNoteRoute = createRoute({
+  method: 'post',
+  path: '/notes/create',
+  request: {
+    body: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: NoteSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    201: {
+      content: {
+        'application/json': {
+          schema: NoteSchema,
+        },
+      },
+      description: 'Create a note',
+    },
+    400: {
+      content: {
+        'application/json': {
+          schema: ErrorSchema,
+        },
+      },
+      description: 'Bad Request',
+    },
+  },
+});
+
+export const noteRoute = createRoute({
+  method: 'get',
+  path: '/notes/{noteKey}',
+  request: {
+    params: NoteParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: NoteSchema,
+        },
+      },
+      description: 'Retrieve the note by noteKey',
+    },
+    400: {
+      content: {
+        'application/json': {
+          schema: ErrorSchema,
+        },
+      },
+      description: 'Bad Request',
+    },
+    404: {
+      content: {
+        'application/json': {
+          schema: ErrorSchema,
+        },
+      },
+      description: 'Not Found',
+    },
+  },
+});
