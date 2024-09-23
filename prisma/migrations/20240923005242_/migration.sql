172cc907b73ebf8858d57f76fdffb694a9f2cf75
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-23 10:10:22 +0900

    Add userId on Note table

--- prisma/migrations/20240923005242_/migration.sql
+++ prisma/migrations/20240923005242_/migration.sql
@@ -3,8 +3,10 @@ CREATE TABLE "Note" (
     "id" TEXT NOT NULL,
     "subject" TEXT NOT NULL,
     "content" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Note_id_key" ON "Note"("id");
