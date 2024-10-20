1ea8f6793cfab1b9c7a0773ebd5aca59ee1b50a3
yocaji <yocaji@stone.sakura.ne.jp>
2024-10-20 21:02:40 +0900

    Remove note ID from body of UPSERT

--- src/api/index.spec.ts
+++ src/api/index.spec.ts
@@ -2,11 +2,11 @@ import { SELF } from 'cloudflare:test';
 
 describe('GET /doc', () => {
   test('レスポンスコードが200であること', async () => {
-    const response = await SELF.fetch('http://example.com/api/doc');
+    const response = await SELF.fetch('https://example.com/api/doc');
     expect(response.status).toBe(200);
   });
   test('OpenAPI Specが表示されること', async () => {
-    const response = await SELF.fetch('http://example.com/api/doc');
+    const response = await SELF.fetch('https://example.com/api/doc');
     const data = await response.json();
     // @ts-expect-error: OpenAPI Specの型を検証する意味がないため
     expect(data.info.title).toBe('SprintPost API');
@@ -15,11 +15,11 @@ describe('GET /doc', () => {
 
 describe('GET /ui', () => {
   test('レスポンスコードが200であること', async () => {
-    const response = await SELF.fetch('http://example.com/api/ui');
+    const response = await SELF.fetch('https://example.com/api/ui');
     expect(response.status).toBe(200);
   });
   test('Swagger UIが表示されること', async () => {
-    const response = await SELF.fetch('http://example.com/api/ui');
+    const response = await SELF.fetch('https://example.com/api/ui');
     const data = await response.text();
     expect(data).toContain('<title>SwaggerUI</title>');
   });
